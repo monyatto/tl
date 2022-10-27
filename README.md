d70cc9182f3ab2a88b0edf5d6ff6db810c535499
monyatto <atsuuu.0925@gmail.com>
2022-10-27 19:38:35 +0900

    readmeに追記

---
+++ README.md
@@ -0,0 +1,8 @@
+# ローカルでアプリケーションを立ち上げるための手順
+
+1. 任意のディレクトリにこのリポジトリのクローンを保存します。
+```
+git clone https://github.com/monyatto/vue.js_ToDo.git
+```
+
+2. index.htmlのフルパスをご使用のブラウザのURL欄に入力してください。
--- index.js
+++ index.js
@@ -1,64 +1,64 @@
 /* globals Vue */
 
 new Vue({
-  el:'#app',
-  data:function() {
-return {
-    addText: '',
-    list: [],
-    todos: [],
-    uid: 0,
-  };
-},
+  el: '#app',
+  data: function () {
+    return {
+      addText: '',
+      list: [],
+      todos: [],
+      uid: 0,
+    };
+  },
   methods: {
-    addTodo: function(){
-      if(this.addText == '') return;
-      this.uid += 1
+    addTodo: function () {
+      if (this.addText == '') return;
+      this.uid += 1;
       let todo = {
-        id :this.uid,
+        id: this.uid,
         text: this.addText,
         check: false,
-        edit: false
-      }
+        edit: false,
+      };
       this.todos.push(todo);
-      localStorage.setItem('uid',this.uid);
+      localStorage.setItem('uid', this.uid);
       this.addText = '';
     },
-    removeTodo: function(todo){
-      var index = this.todos.indexOf(todo)
-      this.todos.splice(index,1)
+    removeTodo: function (todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
     },
-    editTodo: function(todo){
-      todo.edit = !todo.edit
+    editTodo: function (todo) {
+      todo.edit = !todo.edit;
     },
-    checkTodo: function(todo){
-      todo.check = !todo.check
+    checkTodo: function (todo) {
+      todo.check = !todo.check;
     },
-    changeTodo: function(todo){
-      if(todo.text == '') return;
-      todo.edit = false
+    changeTodo: function (todo) {
+      if (todo.text == '') return;
+      todo.edit = false;
     },
   },
   watch: {
     todos: {
-      handler: function() {
-        localStorage.setItem('todos',JSON.stringify(this.todos))
+      handler: function () {
+        localStorage.setItem('todos', JSON.stringify(this.todos));
       },
-      deep: true
-    }
+      deep: true,
+    },
   },
   computed: {
-    allTodos: function(){
-      return this.todos
-    }
+    allTodos: function () {
+      return this.todos;
+    },
   },
-  created(){
-    if(localStorage.getItem('todos')){
-      try{
-        this.todos = JSON.parse(localStorage.getItem('todos'))
-      } catch(e){
-        localStorage.removeItem('todos')
+  created() {
+    if (localStorage.getItem('todos')) {
+      try {
+        this.todos = JSON.parse(localStorage.getItem('todos'));
+      } catch (e) {
+        localStorage.removeItem('todos');
       }
     }
-  }
-})
+  },
+});
